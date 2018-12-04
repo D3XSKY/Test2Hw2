da4cf41a99b075afa3e08475563125851fc24da9
d3xsky <dmisic@gmail.com>
2018-12-04 18:15:25 +0100

    tugy

---
+++ Test2Hw2/Test2Hw2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test2Hw2
+//
+//  Created by Dejan Misic on 04/12/2018.
+//  Copyright © 2018 Dejan Misic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
